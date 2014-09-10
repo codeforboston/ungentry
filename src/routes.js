fa63d755bd21ce04b4e0ab72cd2cf5ffc05e8447
mjrouser <matthew@mjrouser.com>
2014-09-09 21:34:58 -0400

    color brewer

--- src/routes.js
+++ src/routes.js
@@ -37,50 +37,56 @@ var MapRouter = Backbone.Router.extend ({
 
 	  showPct_finance: function(){
 		//setProperty("pctind_finance", "#CCFFCC", "#9900FF"); 
-		setPropertyBrewer("pctind_finance", "BuPu");	
+		setPropertyBrewer("pctind_finance", "PuRd");	
 	  },
 
 	  showPct_professional: function(){
 		//setProperty("pctind_professional", "#FF0033", "#9933FF"); 
-		setPropertyBrewer("pctind_professional", "BuPu"); 
+		setPropertyBrewer("pctind_professional", "PuRd"); 
 	  },
 
 	  showPctocc_profmanage: function(){
-		setProperty("pctocc_profmanage", "#99FF66", "#000099"); 
+		//setProperty("pctocc_profmanage", "#99FF66", "#000099"); 
+		setPropertyBrewer("pctocc_profmanage", "PuRd");
 	  },
 
 	  showPctcollege: function(){
-		setProperty("pctcollege", "#FFFFFF", "#FF3333"); 
+		//setProperty("pctcollege", "#FFFFFF", "#FF3333"); 
+		setPropertyBrewer("pctocc_profmanage", "PuRd");
 	  },
 
 	  showPctinc_0_25k: function(){
-	  	setProperty("pctinc_0_10k", "#99FF66", "#000099");
+	  	//setProperty("pctinc_0_10k", "#99FF66", "#000099");
+	  	setPropertyBrewer("pctinc_0_25k", "GnBu");
 
 	  },
 
 	  showPctinc_25k_50k: function(){
+	  	setPropertyBrewer("pctinc_25k_50k", "GnBu");
 
 	  },
 
 	  showPctinc_50k_100k: function(){
+	  	setPropertyBrewer("pctinc_50k_100k", "GnBu");
 
 	  },
 
 	  showPctinc_100k_more: function(){
+	  	setPropertyBrewer("pctinc_100k_more", "GnBu");
 
 	  },
 
 	  showMedhhincome: function(){
-		setPropertyBrewer("medianhhincome", "OrRd");
+		setPropertyBrewer("medianhhincome", "GnBu");
 	  },
 
 	  showPctpoverty: function(){
 		//setProperty("pctpoverty", "#07E500", "blue"); 
-		setPropertyBrewer("pctpoverty", "BuGn");
+		setPropertyBrewer("pctpoverty", "GnBu");
 	  },
 
 	  showPctpublicassist: function(){
-		setPropertyBrewer("pctpublicassist", "OrRd");
+		setPropertyBrewer("pctpublicassist", "GnBu");
 	  },
 
 	  showUnits: function(){
@@ -100,6 +106,7 @@ var MapRouter = Backbone.Router.extend ({
 	  },
 
 	  showMedianvalue: function() {
+	  	setPropertyBrewer("medianvalue", "OrRd");
 
 	  },
 
@@ -113,26 +120,32 @@ var MapRouter = Backbone.Router.extend ({
 	  },
 
 	  showPctincomerent_30_more: function(){
-
+	  	setPropertyBrewer("pctincomerent_30_more", "OrRd");
 	  },
 
 	  showPctinc_mortgage_30_more: function(){
+	  	setPropertyBrewer("pctinc_mortgage_30_more", "OrRd");
 
 	  },
 
 	  showPctsameres: function(){
+	  	setPropertyBrewer("pctsameres", "OrRd");
+
 
 	  },
 
 	  showPctind_info: function(){
+	  	setPropertyBrewer("pctind_info", "BuPu");
 
 	  },
 
 	  showPctunits_newres: function(){
+	  	setPropertyBrewer("pctunits_newres", "OrRd");
 
 	  },
 
 	  showPctunits_newres_2000: function(){
+	  	setPropertyBrewer("pctunits_newres_2000", "OrRd");
 
 	  }
 
