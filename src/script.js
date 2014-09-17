cb32f697c1795d8780f3e408bd06f350f6832436
mjrouser <matthew@mjrouser.com>
2014-09-16 21:20:26 -0400

    4sq

--- src/script.js
+++ src/script.js
@@ -277,4 +277,25 @@ $(function() {
 });
 
 
+$.ajax({
+	url: 'http://api.foursquare.com/v2/venues/4cd1a0b17f56a1434795d5a6?oauth_token=2D2CXQUH5IWPUSKYIDLWO0CLWPDVLJ0FPCNBS41HYCDKXGCX&v=20140916',
+	type: 'GET',
+	dataType: 'jsonp',
+	data: {param1: 'price'},
+})
+.done(function(data) {
+	console.log(data)
+	console.log(data.response.venue.price.tier);
+	console.log(data.response.venue.location.lat);
+	//for(var venue in data.)
+})
+.fail(function(error) {
+	console.log(error);
+})
+.always(function() {
+	console.log("complete");
+});
+
+
+
 
