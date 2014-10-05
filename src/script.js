29302b5ba086534f7134a226aee273d297b8beaa
mjrouser <matthew@mjrouser.com>
2014-10-04 23:16:53 -0400

    tweak

--- src/script.js
+++ src/script.js
@@ -278,7 +278,7 @@ $(function() {
 
 
 $.ajax({
-	url: 'http://api.foursquare.com/v2/venues/4cd1a0b17f56a1434795d5a6?oauth_token=2D2CXQUH5IWPUSKYIDLWO0CLWPDVLJ0FPCNBS41HYCDKXGCX&v=20140916',
+	url: 'https://api.foursquare.com/v2/venues/4cd1a0b17f56a1434795d5a6?oauth_token=2D2CXQUH5IWPUSKYIDLWO0CLWPDVLJ0FPCNBS41HYCDKXGCX&v=20140916',
 	type: 'GET',
 	dataType: 'jsonp',
 	data: {param1: 'price'},
